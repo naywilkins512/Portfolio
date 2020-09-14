9924470a441b258cc6440610d0e81c1bbcadee6a
naywilkins512 <nathanwilkins01@gmail.com>
2020-09-13 19:02:01 -0500

    adds scrolltrigger functionality

---
+++ assets/script.js
@@ -0,0 +1,74 @@
+gsap.to("#nayphoto", {
+    scrollTrigger: {
+        trigger: ".portfolio-header", 
+        start: "top 95%", 
+        end: "bottom 50%",
+        scrub: true,
+        markers: true,
+        
+    },
+    // x: 400, 
+    opacity: 0, 
+    duration: 1, 
+});
+
+gsap.to(".nay", {
+    scrollTrigger: {
+        trigger: ".portfolio-header", 
+        start: "top", 
+        end: "bottom",
+        scrub: true,     
+        pin: true,   
+    },
+});
+
+gsap.to(".intro", {
+    scrollTrigger: {
+        trigger: ".portfolio-header", 
+        start: "top 100%", 
+        end: "bottom 50%",
+        scrub: true,        
+    },
+    x: -300, 
+    opacity: 0, 
+    duration: 3, 
+});
+
+gsap.to(".honor", {
+    scrollTrigger: {
+        trigger: ".portfolio-header", 
+        start: "top 100%", 
+        end: "bottom 10%",
+        scrub: true,        
+    },
+    x: 300, 
+    opacity: 0, 
+    duration: 3, 
+});
+
+gsap.from(".portfolio-text", {
+    scrollTrigger: {
+        trigger: ".portfolio-header", 
+        start: "top 100%", 
+        end: "bottom top",
+        scrub: true,   
+    },
+
+    opacity: 0, 
+    duration: 3, 
+    // toggleActions: "restart pause reverse reset"
+});
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '#portfolio',
+        start: "center bottom",
+        scrub: true,
+        
+
+    }
+});
+
+tl.from(".portfolio-header", { opcaity: 0, duration: 1})
+   .from(".portfolio", {x: 600, opacity: 0, duration: 0.5}, "-=2")
+   
